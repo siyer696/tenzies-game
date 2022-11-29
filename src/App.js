736d90cd38fe82de1db20211589cfdf46ed3f770
siyer696 <sachiniyermalkapur@gmail.com>
2022-11-29 22:52:02 +0530

    die styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import './App.css';
+import "./App.css";
+import Die from "./components/Die";
 
 function App() {
   return (
     <main>
-      Inside Main
+      <div className="dice--container">
+        <Die value="1" />
+        <Die value="2" />
+        <Die value="3" />
+        <Die value="4" />
+        <Die value="5" />
+        <Die value="6" />
+        <Die value="1" />
+        <Die value="1" />
+        <Die value="1" />
+        <Die value="1" />
+      </div>
     </main>
   );
 }
