fa7024258a2c7a2f13feb72158e27c4eecf25ff7
siyer696 <sachiniyermalkapur@gmail.com>
2022-12-01 22:20:16 +0530

    dynamic dice load

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import "./App.css";
 import Die from "./components/Die";
+import React from "react";
 
 function App() {
+  const [dice, setDice] = React.useState(allNewDice());
+
+  function allNewDice() {
+    let diceArray = [];
+    for (let i = 0; i < 10; i++) {
+      diceArray.push(Math.floor(Math.random() * 6 + 1));
+    }
+    console.log(diceArray);
+    return diceArray;
+  }
+
+  const diceElements = dice.map((die) => {
+    return <Die value={die} />;
+  });
+
   return (
     <main>
-      <div className="dice--container">
-        <Die value="1" />
-        <Die value="2" />
-        <Die value="3" />
-        <Die value="4" />
-        <Die value="5" />
-        <Die value="6" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-        <Die value="1" />
-      </div>
+      <div className="dice--container">{diceElements}</div>
     </main>
   );
 }
