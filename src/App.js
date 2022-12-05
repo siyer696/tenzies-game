9e03c5afc2570b634e795865210f9e43fb72e77f
siyer696 <sachiniyermalkapur@gmail.com>
2022-12-05 21:43:03 +0530

    isHeld styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Die from "./components/Die";
 import React from "react";
+import { nanoid } from "nanoid";
 
 function App() {
   const [dice, setDice] = React.useState(allNewDice());
@@ -8,7 +9,12 @@ function App() {
   function allNewDice() {
     let diceArray = [];
     for (let i = 0; i < 10; i++) {
-      diceArray.push(Math.floor(Math.random() * 6 + 1));
+      diceArray.push({
+        value: Math.floor(Math.random() * 6 + 1),
+        isHeld: false,
+        //this is included as without key, die component gives warning for map
+        id: nanoid(),
+      });
     }
     console.log(diceArray);
     return diceArray;
@@ -19,13 +25,15 @@ function App() {
   }
 
   const diceElements = dice.map((die) => {
-    return <Die value={die} />;
+    return (<Die key={die.id} value={die.value} isHeld={die.isHeld} />);
   });
 
   return (
     <main>
       <div className="dice--container">{diceElements}</div>
-      <button onClick={rollDice} className="roll--button">Roll</button>
+      <button onClick={rollDice} className="roll--button">
+        Roll
+      </button>
     </main>
   );
 }
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const Die = (props) => {
+  const style = {
+    backgroundColor: props.isHeld ? "#59E391" : "white",
+  };
   return (
-    <div className='die--face'>
-        <h2 className='die--num'>{props.value}</h2>
+    <div className="die--face" style={style}>
+      <h2 className="die--num">{props.value}</h2>
     </div>
-  )
-}
+  );
+};
 
-export default Die
+export default Die;
