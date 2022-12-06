2d987460e2c3ae454ec07328305762f1d3629413
siyer696 <sachiniyermalkapur@gmail.com>
2022-12-06 19:39:46 +0530

    added memory to remember state change

--- src/App.js
+++ src/App.js
@@ -3,20 +3,41 @@ import Die from "./components/Die";
 import React from "react";
 import { nanoid } from "nanoid";
 
+
 function App() {
   const [dice, setDice] = React.useState(allNewDice());
+  const [tenzies, setTenzies] = React.useState(false);
+
+  React.useEffect(() => {
+    let check = true;
+    // we can also use .every function of array instead
+    // const allHeld = dice.every(die => die.isHeld)
+    for (let i = 0; i < dice.length; i++) {
+      if (dice[i].isHeld == false || dice[i].value != dice[0].value) {
+        check = false;
+      }
+    }
+    if (check) {
+      setTenzies(true);
+      console.log("You won");
+    }
+    console.log("Dice state changed");
+  }, [dice]);
+
+  function generateNewDie() {
+    return {
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid(),
+    };
+  }
 
   function allNewDice() {
     let diceArray = [];
     for (let i = 0; i < 10; i++) {
-      diceArray.push({
-        value: Math.floor(Math.random() * 6 + 1),
-        isHeld: false,
-        //this is included as without key, die component gives warning for map
-        id: nanoid(),
-      });
+      diceArray.push(generateNewDie());
     }
-    console.log(diceArray);
+    // console.log(diceArray);
     return diceArray;
   }
 
@@ -32,7 +53,11 @@ function App() {
   }
 
   function rollDice() {
-    setDice(allNewDice());
+    setDice((oldDice) =>
+      oldDice.map((die) => {
+        return die.isHeld ? die : generateNewDie();
+      })
+    );
   }
 
   const diceElements = dice.map((die) => {
@@ -49,6 +74,11 @@ function App() {
 
   return (
     <main>
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </p>
       <div className="dice--container">{diceElements}</div>
       <button onClick={rollDice} className="roll--button">
         Roll
